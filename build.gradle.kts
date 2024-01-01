993f98c289529c45ec80c77cbcd57e9465e4e52c
wlm2012 <541253785@qq.com>
2024-01-01 11:30:01 +0800

    gradle

---
+++ build.gradle.kts
@@ -0,0 +1,52 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("org.hibernate.orm") version "6.4.1.Final"
+	id("org.graalvm.buildtools.native") version "0.9.28"
+}
+
+group = "com.study"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation(kotlin("script-runtime"))
+	implementation("com.lmax:disruptor:4.0.0")
+	implementation("org.jsoup:jsoup:1.17.2")
+	implementation("org.apache.httpcomponents.client5:httpclient5:5.3")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+hibernate {
+	enhancement {
+		enableAssociationManagement.set(true)
+	}
+}
+
